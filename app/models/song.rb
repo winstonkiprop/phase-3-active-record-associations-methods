293a279b8ff50b47d7dbbaf64d1def5c7c7c90d3
winston kiprop <winstonekip50@gmail.com>
2022-08-30 11:18:57 +0300

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,6 +3,7 @@ class Song < ActiveRecord::Base
   belongs_to :genre
 
   def get_genre_name
+    self.genre.name
   end
 
   def drake_made_this
@@ -10,5 +11,6 @@ class Song < ActiveRecord::Base
     # Drake doesn't exist in the database as an artist yet, so you'll have to create a record
     # Hint: you won't want to create an artist record every time this method is called, only if Drake is *not found*
 
+self.find_artist artists: "Drake" 
   end
-end
+end 
